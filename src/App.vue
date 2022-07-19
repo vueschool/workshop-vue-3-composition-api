a4b51650ee9b8c83c3010ee59d5b0e151b495c4f
Daniel Kelly <me@danielkelly.io>
2022-07-19 16:01:33 -0500

    solution to exercise 7

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
-import HelloWorld from "@/components/HelloWorld.vue";
+import VideoPlayer from "./components/VideoPlayer.vue";
 </script>
 <template>
   <div id="app">
-    <HelloWorld msg="Rick & Morty Characters" />
+    <VideoPlayer
+      style="width: 800px; margin: 0 auto"
+      src="https://storage.coverr.co/videos/PLhBHZoVCsmGDwDP9fdad01tjZG00w5Dr02?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6Ijg3NjdFMzIzRjlGQzEzN0E4QTAyIiwiaWF0IjoxNjU4MjYyNTQ5fQ.OJAAyQkXoTasm6jNmXwDrz6BHGsLrlYqhVT3laTETuU"
+    />
   </div>
 </template>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+defineProps({
+  src: String,
+});
+
+const video = ref(null);
+</script>
+
+<template>
+  <div>
+    <video :src="src" ref="video" />
+    <div v-if="video">
+      <button @click="video.play()">Play</button>
+      <button @click="video.pause()">Pause</button>
+    </div>
+  </div>
+</template>
