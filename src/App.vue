ebd86fef866c2d5f42b7701c865f84e047b80045
Daniel Kelly <me@danielkelly.io>
2022-07-19 10:28:06 -0500

    solution for exercise 2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
+<script setup>
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
 <template>
   <div id="app">
     <HelloWorld msg="Rick & Morty Characters" />
   </div>
 </template>
-
-<script>
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "app",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
