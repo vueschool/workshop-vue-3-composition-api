3ca72060109ca8a2c3a51826589ef8d79aa562de
Daniel Kelly <me@danielkelly.io>
2022-07-19 16:09:36 -0500

    exercise 8 solution

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import VideoPlayer from "./components/VideoPlayer.vue";
 </script>
 <template>
-  <div id="app">
+  <div id="app" style="height: 2000px">
     <VideoPlayer
       style="width: 800px; margin: 0 auto"
       src="https://storage.coverr.co/videos/PLhBHZoVCsmGDwDP9fdad01tjZG00w5Dr02?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6Ijg3NjdFMzIzRjlGQzEzN0E4QTAyIiwiaWF0IjoxNjU4MjYyNTQ5fQ.OJAAyQkXoTasm6jNmXwDrz6BHGsLrlYqhVT3laTETuU"
--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { ref } from "vue";
+import { useGlobalEvent } from "@/composables/useGlobalEvent";
 defineProps({
   src: String,
 });
 
 const video = ref(null);
+useGlobalEvent("keypress", (e) => {
+  e.preventDefault();
+  if (e.key !== " ") return;
+  video.value.paused ? video.value.play() : video.value.pause();
+});
 </script>
 
 <template>
