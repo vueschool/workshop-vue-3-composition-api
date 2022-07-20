521edd1804468c207df32dbf0d7310f1c65e2a1f
SinBirb <s.birbalta@gmail.com>
2022-07-20 19:30:23 +0200

    Exercise 7

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
+    <VideoPlayer src="/dog-on-the-beach.mp4" />
     <HelloWorld msg="Rick & Morty Characters" />
   </div>
 </template>
 
 <script setup>
 import HelloWorld from "@/components/HelloWorld.vue";
+import VideoPlayer from "./components/VideoPlayer.vue";
 </script>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="m-auto container flex justify-center flex-wrap mt-10">
+    <div class="card-inner">
+      <div class="content text-center">
+        <video ref="video" loop muted playsinline :src="src" width="600" />
+        <button :class="buttonClass" @click="playPause()">
+          {{ buttonLabel }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { computed } from "vue";
+
+defineProps({
+  src: String,
+});
+
+const video = ref(null);
+const isPaused = ref(true);
+
+console.log(video);
+
+onMounted(() => {
+  console.log(video);
+  video.value.play();
+  isPaused.value = video.value?.paused;
+});
+
+const buttonClass = computed(() => {
+  console.log("calc", isPaused.value);
+  return "btn mr-4 " + isPaused.value ? "bg-blue-500" : "bg-red-500";
+});
+
+const buttonLabel = computed(() => {
+  return isPaused.value ? "Play" : "Pause";
+});
+
+const playPause = () => {
+  if (isPaused.value) {
+    video.value.play();
+  } else {
+    video.value.pause();
+  }
+  isPaused.value = !isPaused.value;
+};
+</script>
