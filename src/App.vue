091f4280b6750be259f06726e435f72bbcbb1252
SinBirb <s.birbalta@gmail.com>
2022-07-20 20:22:18 +0200

    Add toggle component buttton

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
+    <button @click="show = !show">Toggle</button>
     <VideoPlayer src="/dog-on-the-beach.mp4" />
-    <HelloWorld msg="Rick & Morty Characters" />
+    <HelloWorld v-if="show" msg="Rick & Morty Characters" />
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 import VideoPlayer from "./components/VideoPlayer.vue";
+
+const show = ref(true);
 </script>
